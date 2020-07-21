b57bdce4566c5c666a9fe88132e91ffdad3c7814
EmberLynn <ember.robb@gmail.com>
2020-07-21 09:54:28 -0600

    Added two simple components

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserOutput = () => {
+    return(
+        <p>This is a little project to test that I know the basic syntax of React!</p>
+    )
+}
+
+export default UserOutput
