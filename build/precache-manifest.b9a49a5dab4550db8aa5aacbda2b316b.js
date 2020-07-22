15e87949157f89c2f2abade7a0b74ee3174ccb33
EmberLynn <ember.robb@gmail.com>
2020-07-22 10:03:23 -0600

    Redeploy

---
+++ build/precache-manifest.b9a49a5dab4550db8aa5aacbda2b316b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8e29d36b905f6201174dae0a8e65c8",
+    "url": "/base-syntax-assignment/index.html"
+  },
+  {
+    "revision": "c5bd2c964863d38b4ea6",
+    "url": "/base-syntax-assignment/static/css/main.a7a23b73.chunk.css"
+  },
+  {
+    "revision": "0d4fa722391950d551df",
+    "url": "/base-syntax-assignment/static/js/2.afe130fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/base-syntax-assignment/static/js/2.afe130fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5bd2c964863d38b4ea6",
+    "url": "/base-syntax-assignment/static/js/main.8112c636.chunk.js"
+  },
+  {
+    "revision": "db625e42f3e8ae577e6a",
+    "url": "/base-syntax-assignment/static/js/runtime-main.348c65d7.js"
+  }
+]);
