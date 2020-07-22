28a8630882461590748ece6bca55590f7124a0c1
EmberLynn <ember.robb@gmail.com>
2020-07-22 09:24:16 -0600

    Updates

---
+++ precache-manifest.c9556a1bc2fc1460fbdbc866a94575ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617cdd50f9587ec3a59cc6dfe27d4c34",
+    "url": "/base-syntax-assignment/index.html"
+  },
+  {
+    "revision": "c75f81b272703bf8fbd6",
+    "url": "/base-syntax-assignment/static/css/main.ca66711f.chunk.css"
+  },
+  {
+    "revision": "0d4fa722391950d551df",
+    "url": "/base-syntax-assignment/static/js/2.afe130fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/base-syntax-assignment/static/js/2.afe130fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75f81b272703bf8fbd6",
+    "url": "/base-syntax-assignment/static/js/main.28f8efdb.chunk.js"
+  },
+  {
+    "revision": "db625e42f3e8ae577e6a",
+    "url": "/base-syntax-assignment/static/js/runtime-main.348c65d7.js"
+  }
+]);
